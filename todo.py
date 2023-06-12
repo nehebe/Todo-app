2d01da1550e32665dcf541bc49297933193269ea
chris will <nehebe@outlook.com>
2023-06-12 01:01:08 -0400

    commit

--- todo.py
+++ todo.py
@@ -5,11 +5,13 @@ import InquirerPy
 conn = sqlite3.connect("todo.db")
 c = conn.cursor()
 
-# c.execute("""CREATE TABLE todo (
-# 			thing_todo text,
-# 			is_it_done text,
-# 			time_it_was_made text
-# 			)""")
+
+# comment this out after you have the .db file
+c.execute("""CREATE TABLE todo (
+			thing-todo text,
+			is-it-done text,
+			time-it-was-made text
+			)""")
 
 screen = input("R for read, W for write: ")
 
