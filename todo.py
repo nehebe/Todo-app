57aadc96e7945969b7937e59f2f54de1961a1191
chris will <nehebe@outlook.com>
2023-06-13 23:48:27 -0400

    fix

--- todo.py
+++ todo.py
@@ -1,28 +1,23 @@
-import os
 import sqlite3
 import datetime
 from InquirerPy import prompt
 from InquirerPy.base.control import Choice
 from InquirerPy import inquirer
-from InquirerPy.separator import Separator
 from InquirerPy.validator import EmptyInputValidator
-from InquirerPy.validator import PathValidator
-from InquirerPy.validator import NumberValidator
 
-things = []
 
-questions_1 = [
-    {
-        "type": "filepath",
-        "message": "Enter file to upload:",
-        "name": "location",
-        "default": os.getcwd(),
-        "validate": PathValidator(is_file=True, message="Input is not a file"),
-        "only_files": True,
-    },
-]        
+conn = sqlite3.connect("todo.db")
+c = conn.cursor()
 
-question_2 = [
+
+# comment this out after you have the .db file
+c.execute("""CREATE TABLE todo (
+			thing_todo text,
+			is_it_done text,
+			time_it_was_made text
+			)""")
+
+question_1 = [
 	{
 		"type": "list",
 		"message": "Select an action:",
@@ -31,42 +26,7 @@ question_2 = [
 	},
 ]
 
-question_3 = [
-	{
-		"type": "rawlist",
-		"choices": [],
-		"message": "Press Ctrl+C to exit",
-		"default": 1,
-	},
-	{
-		"type": "rawlist",
-		 "message": "Press Ctrl+C to exit",
-		"choices": [
-			Choice(name="Finished task", value="fin"),
-			Choice(name="Delete task", value="del"),
-			Separator(line=15 * "*"),
-			Choice(name="Go back", value="bak"),
-		],
-		"validate": lambda result: len(result) > 0,
-		"invalid_message": "Minimum 1 selection",
-    },			
-]
-
-db_location = prompt(questions_1)
-conn = sqlite3.connect(db_location["location"])
-c = conn.cursor()
-
-
-# comment this out after you have the .db file
-# c.execute("""CREATE TABLE todo (
-# 			thing_todo text,
-# 			is_it_done text,
-# 			time_it_was_made text
-# 			)""")
-
-
-result = prompt(questions=question_2)
-action = result[0]
+result = prompt(questions=question_1)
 
 if result[0] == "Write":
 	todo = inquirer.text(message="What would you like to put in the todo list:", validate=EmptyInputValidator(),).execute()
@@ -75,12 +35,6 @@ if result[0] == "Write":
 	c.execute("INSERT INTO todo VALUES (?, ?, ?)", (todo, is_done, time_made))
 elif result[0] == "Read":
 	c.execute("SELECT * FROM todo")
-	things = c.fetchall()
-	question_3[0]["choices"] = things
-	readlist = prompt(question_3)
+	print(c.fetchall())
 else:
 	exit()
-
-
-conn.commit()
-conn.close()
